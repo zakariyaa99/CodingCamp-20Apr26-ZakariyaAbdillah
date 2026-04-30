ce3d6dd25e5592f7e522d72da42f5401303dcfba
zakariyaa99 <zakariyaa99@github.com>
2026-04-30 15:00:58 +0700

    Initial commit from Create Next App

---
+++ src/app/layout.js
@@ -0,0 +1,37 @@
+import Script from 'next/script';
+
+export const metadata = {
+  title: 'Anon - eCommerce Website',
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="UTF-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <link rel="shortcut icon" href="/images/logo/favicon.ico" type="image/x-icon" />
+        <link rel="stylesheet" href="/css/style-prefix.css" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800;900&display=swap"
+          rel="stylesheet"
+        />
+      </head>
+      <body>
+        {children}
+        <Script
+          type="module"
+          src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"
+          strategy="afterInteractive"
+        />
+        <Script
+          noModule
+          src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"
+          strategy="afterInteractive"
+        />
+      </body>
+    </html>
+  );
+}
